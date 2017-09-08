93305d7010fdd2df385723d096eae35c99317ca1
Jesús Alvarado Garzón <xtornasol512@gmail.com>
2017-09-08 13:43:37 -0500

    add v-for to prescriptions

---
+++ statics/js/app.js
@@ -0,0 +1,16 @@
+// Main app js
+var app = new Vue({
+  el: '#app-prescription',
+  data: {
+    items : [
+      {
+        id: 1,
+        medic: "Jesus Alvarado Garzon",
+        patient : "Normal Guy",
+        diagnosis: "Diagnostico de prueba",
+        print_url: "print_url"
+
+      }
+    ]
+  }
+})
