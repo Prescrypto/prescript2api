e6e67df610a15ac720c7e54c26805f47587e5d3f
Jesús Alvarado Garzón <xtornasol512@gmail.com>
2017-09-08 13:50:04 -0500

    add minimal css adjust

--- statics/js/app.js
+++ statics/js/app.js
@@ -10,6 +10,22 @@ var app = new Vue({
         diagnosis: "Diagnostico de prueba",
         print_url: "print_url"
 
+      },
+      {
+        id: 2,
+        medic: "Cesar Mejia",
+        patient : "Normal Guy",
+        diagnosis: "Retinopatia Diabetica ",
+        print_url: "print_url"
+
+      },
+      {
+        id: 3,
+        medic: "Bernardo Mesa",
+        patient : "Normal Guy",
+        diagnosis: "Ojo irritado",
+        print_url: "print_url"
+
       }
     ]
   }
