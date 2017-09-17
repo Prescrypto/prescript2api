bcdd7462df255f3baa4d88996d2e91db8263039f
Jesús Alvarado Garzón <xtornasol512@gmail.com>
2017-09-17 12:56:53 -0500

    add external patient file with hidden option

--- statics/js/app.js
+++ statics/js/app.js
@@ -2,24 +2,28 @@
 
 //Main reference to API
 const RX_ENDPOINT = 'https://prescrypto-development.herokuapp.com/api/v1/rx-endpoint/';
-const MEDIC_TOKEN = '4b697691a633a95ae907c4958ccd9e9748a72a56';
+const MEDIC_TOKEN = '4b697691a633a95ae907c4958ccd9e9748a72a56'; // replace with yours medic token
 const PRINT_URL = 'https://prescrypto-development.herokuapp.com/new/api_token?token=' + MEDIC_TOKEN + '&redirect_url=/print/';
 const headers_ = {'Authorization': 'Token ' + MEDIC_TOKEN };
-var rxs = [];
-var rx_item = {};
+// globally variables
+var rxs = []; // Lista de Prescripciones
+var rx_item = {}; // Item individual de prescripcion
 
+
+// App del formulario ligado al elemento #app-create-rx
 var rxform = new Vue({
   el: '#app-create-rx',
   data: {
     patient: {
       name : "wozowsky",
       email : "wozowsky1234",
-      date_of_birth: "1988-09-09"
+      date_of_birth: "1988-09-09",
+      external_patient_file: "hidden_id_by_host_hospital"
     },
     diagnosis: 'Diagnostico',
     extras: 'Extras',
     medications: [],
-    clinic: null
+    clinic: null // if not exist clinics just put null
   },
   methods: {
     sign_and_send : function(event){
